4f0af184ee28e131d58a27f7602a484bab4d17bd
bj322-design <brandonschadoff@gmail.com>
2026-03-24 12:42:12 -0400

    Renamed Tests -> TestCases.java to resolve naming issues in IDE

---
+++ TestCases.java
@@ -0,0 +1,56 @@
+import org.junit.*;
+
+public class TestCases {
+
+    @Test
+    public void Test1() {
+        //PLEASE USE ASSERTIONS TO WRITE TESTS THIS WAS IN PLACE TO SET UP THE FILE AND MAKE SURE JUNITS ARE WORKING
+        int a = 1;
+        Assert.assertEquals(1, a);
+    }
+
+    @Test
+    public void Test2() {
+
+    }
+
+    @Test
+    public void Test3() {
+
+    }
+
+    @Test
+    public void Test4() {
+
+    }
+
+    @Test
+    public void Test5() {
+
+    }
+
+    @Test
+    public void Test6() {
+
+    }
+
+    @Test
+    public void Test7() {
+
+    }
+
+    @Test
+    public void Test8() {
+
+    }
+
+    @Test
+    public void Test9() {
+
+    }
+
+    @Test
+    public void Test10() {
+
+    }
+}
