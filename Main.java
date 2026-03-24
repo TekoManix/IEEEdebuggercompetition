0c6637d68c3a323a7c79abdbda045a6b6e98fc1b
bj322-design <brandonschadoff@gmail.com>
2026-03-24 12:32:50 -0400

    Added structure to main, tests files

--- Main.java
+++ Main.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Main {
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * Debug competition for IEEE. There will be 10 Tasks of different variety.
+ * Task: You are going to make algorithms and use the debugger to solve the issues
+ * Some will utilize external data in txt files. If that is the case we will provide needed setup of libs like scanner and File
+ * (DO NOT CHANGE THERE OR THE METHOD HEADERS)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        task1();
+
+    }
+
+    public static void task1(){
+        try {
+            File f = new File("data.txt");
+            Scanner file = new Scanner(f);
+
+
+
+
+
+
+
+
+        }catch (Exception e){
+            System.out.println("Can't open file, 'data.txt'");
+        }
+    }
+
+    public static void task2(){
+
+    }
+
+    public void task3(){
+
+    }
+
+    public void task4(){
+
+    }
+
+    public void task5(){
+
+    }
+
+    public void task6(){
+
+    }
+
+    public void task7(){
+
+    }
+
+    public void task8(){
+
+    }
+
+    public void task9(){
+
+    }
+
+    public void task10(){
+
+    }
 }
